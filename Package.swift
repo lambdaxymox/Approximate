49b0772bf3427d94def106e176355daf9bf89616
LambdaXymox <lambda.xymox@gmail.com>
2022-05-31 07:17:05 -0500

    Flatten source tree a little.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,6 @@ import PackageDescription
 let package = Package(
     name: "Approximate",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Approximate",
             targets: ["Approximate"]),
@@ -14,11 +13,12 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Approximate",
-            dependencies: []),
+            dependencies: [],
+            path: ".",
+            exclude: ["README.md", "Tests", "LICENSE-APACHE", "LICENSE-MIT"],
+            sources: ["Sources"]),
         .testTarget(
             name: "ApproximateTests",
             dependencies: ["Approximate"]),
